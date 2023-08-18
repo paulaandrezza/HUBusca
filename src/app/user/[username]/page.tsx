6c73384de1abe361c0913ed7348c6bbea6bb91d2
paulaandrezza <paulaandrezza25@gmail.com>
2023-08-18 14:08:59 -0300

    making request again

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -4,18 +4,20 @@ import { useUserContext } from '@/app/contexts/UserContext';
 import { Aside } from '@/layouts/Aside';
 import { UserMain } from '@/layouts/UserMain';
 import { getUserProfile } from '@/services/users/getUserProfile';
+import { useEffect } from 'react';
 
 export default function Page({ params }: { params: { username: string } }) {
   const { userInfo, setUserInfo } = useUserContext();
 
   // TODO: remover useContext pois requisitar novamente garante que o link sempre funcione
-  const handlegetUserProfile = async () => {
-    if (!userInfo) {
+  useEffect(() => {
+    const handlegetUserProfile = async () => {
       const response = await getUserProfile(params.username);
       setUserInfo(response);
-    }
-  };
-  handlegetUserProfile();
+    };
+
+    handlegetUserProfile();
+  }, [params.username]);
 
   return (
     <>
