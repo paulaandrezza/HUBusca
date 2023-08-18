1dfe312f549d14bee146407034b08739b48ad281
paulaandrezza <paulaandrezza25@gmail.com>
2023-08-18 14:09:32 -0300

    doing dynamic recent searches

--- src/layouts/Aside/index.tsx
+++ src/layouts/Aside/index.tsx
@@ -1,9 +1,20 @@
 import { Text, Wrapper, WrapperArea } from '@/app/theme/sharedStyles';
 import { SmallCard } from '@/components/SmallCard';
+import { IUser } from '@/services/interfaces/User';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import { AsideContainer, RightAlign } from './style';
 
 export const Aside = () => {
+  const [userInfo, setUserInfo] = useState<IUser[]>();
+
+  useEffect(() => {
+    const recentProfiles = JSON.parse(
+      localStorage.getItem('recentProfiles') || '[]'
+    );
+    setUserInfo(recentProfiles);
+  }, []);
+
   return (
     <AsideContainer>
       <WrapperArea>
@@ -17,7 +28,9 @@ export const Aside = () => {
         </RightAlign>
         <Wrapper $biggerGap>
           <Text>Pesquisas Recentes:</Text>
-          <SmallCard />
+          {userInfo?.map((user: IUser) => (
+            <SmallCard key={user.login} userInfo={user} />
+          ))}
         </Wrapper>
       </WrapperArea>
     </AsideContainer>
