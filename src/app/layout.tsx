fe4c982f84087a04d81a0ed5ebb72923b941eae0
paulaandrezza <paulaandrezza25@gmail.com>
2023-08-19 23:37:38 -0300

    remove metadata export from layout to fix vercel deploy error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 'use client';
 
 import StyledComponentsRegistry from '@/lib/registry';
-import type { Metadata } from 'next';
 import { Quicksand } from 'next/font/google';
 import GlobalStyle from './theme/globalStyles';
 
 const quicksand = Quicksand({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'HUBusca',
-  description: 'Site de busca de repositórios dos devs',
-};
 
 export default function RootLayout({
   children,
